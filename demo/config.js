f74f04d124840d47727194cd233239571165974c
KevonParisian <egzmdiwfvx@outlook.com>
2023-12-13 00:20:46 +0800

    dolor

---
+++ demo/config.js
@@ -0,0 +1,9 @@
+var config = {
+    SecretId: process.env.SecretId || '',
+    SecretKey: process.env.SecretKey || '',
+    Bucket: process.env.Bucket || '',
+    Region: process.env.Region || '',
+    Uin: process.env.Uin || '',
+};
+
+module.exports = config;
